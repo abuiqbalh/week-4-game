1670cfac37acf50180226a8fb32112bdeee99165
abuiqbalh <33363982+abuiqbalh@users.noreply.github.com>
2018-02-28 11:05:51 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -28,25 +28,47 @@ $(document).ready(function(){
         //if ($("#1").click()===true){gameScore+=$("#1").attr("value");}*/
         //if(!bEventSet)
         {
-        $("#1").click(function(){ gameScore=gameScore+parseInt($("#1").attr("value"));$("#score").html("<b>"+gameScore+"</b>");
+        $("#1").click(function(){ gameScore=gameScore+parseInt($("#1").attr("value"));$("#score").html("<b>"+gameScore+"</b>"); 
+        if(gameScore===targetNumber){wins+=1; gameScore=0;$("#target").text(Math.floor(Math.random()*33+18));console.log(wins);}
+        else if (gameScore>targetNumber){loss+=1; gameScore=0; $("#target").text(Math.floor(Math.random()*33+18));console.log(loss);}
+        $("#wins").text(wins);
+        $("#loss").text(loss);
         });
         $("#2").click(function(){ gameScore=gameScore+parseInt($("#2").attr("value")); $("#score").html("<b>"+gameScore+"</b>");
+        if(gameScore===targetNumber){wins+=1; gameScore=0;$("#target").text(Math.floor(Math.random()*33+18));console.log(wins);}
+        else if (gameScore>targetNumber){loss+=1; gameScore=0;$("#target").text(Math.floor(Math.random()*33+18));console.log(loss);}
+        $("#wins").text(wins);
+        $("#loss").text(loss);
         });
         $("#3").click(function(){ gameScore=gameScore+parseInt($("#3").attr("value")); $("#score").html("<b>"+gameScore+"</b>");
+        if(gameScore===targetNumber){wins+=1; gameScore=0;$("#target").text(Math.floor(Math.random()*33+18));console.log(wins);}
+        else if (gameScore>targetNumber){loss+=1; gameScore=0; $("#target").text(Math.floor(Math.random()*33+18));console.log(loss);}
+        $("#wins").text(wins);
+        $("#loss").text(loss);
         });
         $("#4").click(function(){ gameScore=gameScore+parseInt($("#4").attr("value")); $("#score").html("<b>"+gameScore+"</b>");
+        if(gameScore===targetNumber){wins+=1; gameScore=0;$("#target").text(Math.floor(Math.random()*33+18));console.log(wins);}
+        else if (gameScore>targetNumber){loss+=1; gameScore=0; $("#target").text(Math.floor(Math.random()*33+18));console.log(loss);}
+        $("#wins").text(wins);
+        $("#loss").text(loss);
         });
         //beventSet=true;
+        }
+
+        if (gameScore===targetNumber||gameScore>targetNumber){
+
+            targetNumber=0;
+            gameScore=0;
         }
         //console.log(gameScore);
         //console.log($("#score").attr());
        // console.log($("score"))
         //console.log(point);
-        console.log($.parseHTML( $("#score") ));
-        if(gameScore===targetNumber){wins+=1; gameScore=0;targetNumber=0;console.log(wins);}
+        //console.log($.parseHTML( $("#score") ));
+        /*if(gameScore===targetNumber){wins+=1; gameScore=0;targetNumber=0;console.log(wins);}
         else if (gameScore>targetNumber){loss+=1; gameScore=0; console.log(loss);}
         $("#wins").text(wins);
-        $("#loss").text(loss);
+        $("#loss").text(loss);*/
         //$("#1").click(function(){ alert(gameScore=gameScore+parseInt($("#1").attr("value")))});
         //console.log(gameScore);
     /* gameScore+=function({2
